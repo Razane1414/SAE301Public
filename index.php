e6942edb062a3fc679b5d80d531652270b65d9e3
Razane1414 <razanefeggous@gmail.com>
2024-12-11 16:26:12 +0100

    Ajout de la page index.php (navbar uniquement)

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Team Vulcan</title>
+    <link rel="stylesheet" href=css/home.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+
+<body>
+    <header>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-4">
+          <img src="votre-logo.png" alt="Logo" class="img-fluid" style="max-width: 150px;">
+        </div>
+
+        <!-- Navigation au centre -->
+        <div class="col-4 text-center">
+          <nav class="nav">
+            <a class="nav-link active" href="#">LE CLUB</a>
+            <a class="nav-link" href="#">SPÉCIALITÉS</a>
+            <a class="nav-link" href="#">AVIS CLIENTS</a>
+            <a class="nav-link" href="#">BLOG</a>
+          </nav>
+        </div>
+
+        <div class="col-4 text-end">
+          <a href="#" class="btn-rejoindre btn me-2">Nous rejoindre</a>
+          <a href="#" class="text-dark">
+            <img src="public/images/icone_membre.svg" alt="Espace Membre" class="img-fluid" style="max-width: 40px;">
+            </a>
+        </div>
+      </div>
+    </div>
+    </header>
+</body>
+</html>
