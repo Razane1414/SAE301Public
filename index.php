8b23b12e8802e387225df25813d587ba5486c151
Votre Nom <bonhommesimon1@outlook.com>
2024-12-12 14:13:35 +0100

    update index.php

--- index.php
+++ index.php
@@ -4,38 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Team Vulcan</title>
-    <link rel="stylesheet" href=css/home.css">
+    <link rel="stylesheet" href="css\home.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
 </head>
 
 
 <body>
-    <header>
+  <header>
     <div class="container">
-      <div class="row align-items-center">
-        <div class="col-4">
-          <img src="votre-logo.png" alt="Logo" class="img-fluid" style="max-width: 150px;">
-        </div>
+        <div class="row align-items-center">
+            <div class="col-3">
+                <img src="images/Logo1.png" alt="Logo" class="img-fluid" style="max-width: 100px;">
+            </div>
 
-        <!-- Navigation au centre -->
-        <div class="col-4 text-center">
-          <nav class="nav">
-            <a class="nav-link active" href="#">LE CLUB</a>
-            <a class="nav-link" href="#">SPÉCIALITÉS</a>
-            <a class="nav-link" href="#">AVIS CLIENTS</a>
-            <a class="nav-link" href="#">BLOG</a>
-          </nav>
-        </div>
+            <!-- Navigation au centre -->
+            <div class="col-6 text-center">
+                <nav class="nav justify-content-center">
+                    <a class="nav-link mx-2 active" href="#">LE CLUB</a>
+                    <a class="nav-link mx-2" href="#">SPÉCIALITÉS</a>
+                    <a class="nav-link mx-2" href="#">AVIS CLIENTS</a>
+                    <a class="nav-link mx-2" href="#">BLOG</a>
+                </nav>
+            </div>
 
-        <div class="col-4 text-end">
-          <a href="#" class="btn-rejoindre btn me-2">Nous rejoindre</a>
-          <a href="formulaire_connexion.php" class="text-dark">
-            <img src="public/images/icone_membre.svg" alt="Espace Membre" class="img-fluid" style="max-width: 40px;">
-            </a>
+            <div class="col-3 d-flex justify-content-end align-items-center flex-wrap">
+                <!-- Le bouton et l'icône sont dans un conteneur flex -->
+                <a href="#" class="btn-rejoindre btn me-2 mb-2 mb-sm-0">Nous rejoindre</a>
+                <a href="#" class="text-dark">
+                    <img src="images/icone_membre.svg" alt="Espace Membre" class="img-fluid" style="max-width: 40px;">
+                </a>
+            </div>
         </div>
-      </div>
     </div>
-    </header>
+  </header>
+
+  <div class="home">
+      <div class="overlay-text">
+          <h4 class="sous-titre">WELCOM TO THE FIGHT</h4>
+          <h1>TEAM <span class="highlight">VULCAN</span>, <br>PASSION DES SPORTS DE COMBAT</h1>
+          <p>Rejoignez nous dans l'univers des sports de combat avec une équipe professionnelle dédiée à votre progression et à votre épanouissement. Que vous soyez débutant ou confirmé, la team Vulcan vous attend.</p>
+          <a href="#" class="btn-rejoindre btn">BOXER !</a>
+      </div>
+  </div>
+
+    <?php include 'footer.php'; ?>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
-
