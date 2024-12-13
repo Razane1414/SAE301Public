3648c75f18c78f26fb2aa3da65cc72819cb53c5f
AlixBedo <alixbedoy@gmail.com>
2024-12-13 15:34:14 +0100

    Calendrier update 2

--- admin/espace_admin.php
+++ admin/espace_admin.php
@@ -77,6 +77,8 @@ $events = Event::getAllEvents($pdo);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gestion des événements</title>
+    <link rel="stylesheet" href="../include/css/calendrier.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
     <h1>Gestion des événements</h1>
@@ -136,7 +138,51 @@ $events = Event::getAllEvents($pdo);
             <?php endforeach; ?>
         </tbody>
     </table>
+
+    
+    <div class="container py-5">
+        <div class="header">Événement</div>
+        <div class="content-section">
+            <div class="text-section">
+                <div class="event-title">Événement sélectionné : Stage de découverte JJB</div>
+                <p class="description">
+                    L'événement sélectionné est un stage de découverte de Jiu-Jitsu Brésilien (JJB), une opportunité
+                    idéale pour s'initier à cet art martial et en apprendre les bases dans un cadre encadré et convivial
+                    réalisé le Lundi 06.
+                </p>
+
+                <div class="trainer-card">
+                    <img src="../include/images/visage.jpg" alt="Photo de profil">
+                    <div class="trainer-card-content">
+                        <h4><strong>David TIVEYRAT</strong></h4>
+                        <p><strong>Entraîneur JJB, Grappling</strong></p>
+                        <ul>
+                            <li><strong>Ceinture bleue grappling et grappling-fight</strong></li>
+                            <li><strong>Ceinture noire de Judo</strong></li>
+                            <li><strong>Ceinture noire de Ju-Jitsu</strong></li>
+                            <li><strong>Ceinture bleue de Jiu-Jitsu Brésilien</strong></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="button-container">
+                    <button class="btn-register">S'inscrire à l'événement</button>
+                </div>
+            </div>
+
+            <div class="image-section">
+                <div class="image-container">
+                    <img src="../include/images/jjb.png" alt="JJB Stage">
+                    <div class="corner corner-top-left"></div>
+                    <div class="corner corner-bottom-right"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
 
 <?php
